7c6359c785ad11b123306cd9f3edae75199cfee2
Farhan <farhansarang7@gmail.com>
2024-08-22 19:08:30 -0400

    Created Employee-Details Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule,
