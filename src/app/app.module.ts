4c7e2a726ec2ff77fc92f6c50b5fa7061f31c29c
Farhan <farhansarang7@gmail.com>
2024-08-22 19:09:10 -0400

    Created Employee-Forms Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EMployeeFormsComponent } from './employee-forms/employee-forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    EmployeeDetailsComponent
+    EmployeeDetailsComponent,
+    EMployeeFormsComponent
   ],
   imports: [
     BrowserModule,
