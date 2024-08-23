f0fe252f30886539effc55dc0e52553cc6490ec6
Farhan <farhansarang7@gmail.com>
2024-08-22 20:31:32 -0400

    Finished Routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';  
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeSalaryComponent } from './employee-salary/employee-salary.component';
+import { Salary } from './shared/salary.model';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'employee', component:EmployeeDetailsComponent},
+  {path:'salary', component:EmployeeSalaryComponent},
+  {path:'**', redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
