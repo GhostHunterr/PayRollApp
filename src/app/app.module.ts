85cf7f67a90e2487fe2f4017d74116ce4f8b9221
Farhan <farhansarang7@gmail.com>
2024-08-22 20:05:07 -0400

    Nothing Changed..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { EMployeeFormsComponent } from './employee-forms/employee-forms.component';
+import { EmployeeSalaryComponent } from './employee-salary/employee-salary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     EmployeeDetailsComponent,
-    EMployeeFormsComponent
+    EMployeeFormsComponent,
+    EmployeeSalaryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/employee-salary/employee-salary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-salary',
+  templateUrl: './employee-salary.component.html',
+  styles: ``
+})
+export class EmployeeSalaryComponent {
+
+}
